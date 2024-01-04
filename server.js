6ab5d396602f67922023b76054a4020c073f4e6a
Divyank Trivedi <divyanktrivedi0@gmail.com>
2024-01-04 08:31:18 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require("express");
+const errorHandler = require("./middleware/errorHandler");
+const connectDb =require("./config/dbConnection")
+const { connect } = require("mongoose");
+const dotenv = require ("dotenv").config()
+
+
+
+const app =express();
+connectDb();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/contacts",require('./routes/contactRoutes'));
+app.use("/api/users",require('./routes/userRoutes'));
+
+app.use(errorHandler)
+
+app.listen(port , ()=>{
+    console.log(`server running on port ${port}`)
+})  
